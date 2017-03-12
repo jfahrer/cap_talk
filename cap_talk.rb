2be724bc21a8e1f59abda345e160738feea120f9
Julian Fahrer <julian@fahrer.net>
2017-03-11 18:29:58 -0800

    Raise an exception if database.yml does not exist

--- cap_talk.rb
+++ cap_talk.rb
@@ -2,6 +2,8 @@ require 'sinatra/base'
 require 'erb'
 
 class CapTalk < Sinatra::Base
+  class NoDatabaseConfig < StandardError; end
+
   set :raise_errors, true
 
   get '/' do
@@ -9,6 +11,11 @@ class CapTalk < Sinatra::Base
   end
 
   get '/steps/:step' do
+    check_db_config
     erb :steps, locals: { step: params[:step] }
+  end
+
+  def check_db_config
+    File.exist?('config/database.yml') ? true : fail(NoDatabaseConfig)
   end
 end
